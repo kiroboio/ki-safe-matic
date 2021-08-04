51a23e46e5decfb3958f66f25d386029f8274a22
Ori Shalom <oris@kirobo.io>
2021-08-04 09:30:21 +0300

    initial commit

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 //const SafeTransfer = artifacts.require('SafeTransfer')
 const Token = artifacts.require('Token')
-const Pool = artifacts.require('Pool')
+//const Pool = artifacts.require('Pool')
 //const SafeSwap = artifacts.require("SafeSwap")
 //const Staking = artifacts.require("Staking")
 
@@ -14,15 +14,15 @@ module.exports = function(deployer, network, accounts) {
   deployer.then(async () => {
     
     const token   = await deployer.deploy(Token, { from: tokenOwner })
-    const pool    = await deployer.deploy(Pool, token.address, { from: tokenOwner })
-    //const staking    = await deployer.deploy(Staking, token.address, { from: poolOwner })
+    //const pool    = await deployer.deploy(Pool, '0xFcf75F14d14F4B63F5e09e584Aa5282145eE2dF8', { from: tokenOwner })
+    //const staking    = await deployer.deploy(Staking, '0xFcf75F14d14F4B63F5e09e584Aa5282145eE2dF8', { from: poolOwner })
     //const safeTransfer = await deployer.deploy(SafeTransfer ,'0x3E3185B08FF717b7a9126f6E160099dfe145d5E9' , { from: tokenOwner })
     //const safeSwap = await deployer.deploy(SafeSwap ,'0x3E3185B08FF717b7a9126f6E160099dfe145d5E9' , { from: tokenOwner })
-    await token.disableTransfers(false, { from: tokenOwner })
+    //await token.disableTransfers(false, { from: tokenOwner })
      if (liveTestNetworks[network]) {
        const supply = 100000n * 10n**18n
        await token.mint(pool.address, supply.toString(), { from: tokenOwner })
-       await pool.resyncTotalSupply(supply.toString(), { from: poolOwner })
-    } 
+      // await pool.resyncTotalSupply(supply.toString(), { from: poolOwner })
+    }
   })
 }
